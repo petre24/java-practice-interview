846a8c8cd06e1261c3e367eaa2c55e69db476be7
Petre Quintua <pm_quin_24@yahoo.com>
2020-03-10 12:48:12 -0700

    created Fibonacci.java
    displays a desired number of terms in the Fibonacci sequence

---
+++ Fibonacci.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+* author: P. Quintua
+* file: Fibonacci.java
+*
+* description: This is a program which will print Fibonacci series, 
+*               e.g. 1 1 2 3 5 8 13 ... up to a given number.
+******************************************************************************/
+
+import java.util.Scanner;
+
+public class Fibonacci {
+  public static void main(String[] args) {
+  
+    Scanner keyboard = new Scanner(System.in);
+    
+    System.out.print("Input a number to extend the Fibonacci series: ");
+    System.out.println();
+    int numberOfTerms = keyboard.nextInt();
+    
+    System.out.println("Fibonacci series up to " + numberOfTerms + " numbers: ");
+    
+    for (int i = 1; i <= numberOfTerms; i++) {
+      System.out.print(fibonacci(i) + " ");
+    }
+  }
+  
+  public static int fibonacci(int number) {
+    if (number == 1 || number == 2) 
+      return 1;
+      
+    return fibonacci(number - 1) + fibonacci(number - 2);  
+  }
+}
