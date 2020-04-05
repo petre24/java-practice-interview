94a50ea21a7079586655f2626957de072affeac1
petre24 <pm_quin_24@yahoo.com>
2020-04-05 06:25:46 -0700

    closed scanner

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,4 @@
+
 /******************************************************************************
 * author: P. Quintua
 * file: Fibonacci.java
@@ -10,24 +11,26 @@ import java.util.Scanner;
 
 public class Fibonacci {
   public static void main(String[] args) {
-  
+
     Scanner keyboard = new Scanner(System.in);
-    
+
     System.out.print("Input a number to extend the Fibonacci series: ");
     System.out.println();
     long numberOfTerms = keyboard.nextLong();
-    
+
     System.out.println("Fibonacci series up to " + numberOfTerms + " numbers: ");
-    
+
     for (int i = 1; i <= numberOfTerms; i++) {
       System.out.print(fibonacci(i) + " ");
     }
+
+    keyboard.close();
   }
-  
+
   public static int fibonacci(int number) {
-    if (number == 1 || number == 2) 
+    if (number == 1 || number == 2)
       return 1;
-      
-    return fibonacci(number - 1) + fibonacci(number - 2);  
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
   }
 }
